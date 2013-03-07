ece8e1ee5b2f091ddabff49cad167dcfc7adf234
Green Luo <greenlaw110@gmail.com>
2013-03-07 23:47:40 +1100

    make site cache lang sensitive

--- app/controllers/Site.java
+++ app/controllers/Site.java
@@ -1,17 +1,10 @@
 package controllers;
 
 import com.greenlaw110.rythm.play.Cache4;
-import play.Play;
-import play.i18n.Lang;
 import play.mvc.Controller;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-
 public class Site extends Controller {
-    @Cache4
+    @Cache4(langSensitive = true)
     public static void index() {
         render();
     }
